28edb1d361a613f98610441eacec7a8a46b51c92
Atul Rauthan <atulrauthan7@gmail.com>
2020-07-29 20:56:13 +0530

    Fixed bug to remove multiple results from appearing

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,19 @@
 // Challenge 1: Your age in days
 
 function ageInDays() {
+    if(document.getElementById('ageInDays')) {
+        document.getElementById('ageInDays').remove()
+    }
     var birthYear = prompt('What year were you born in?')
     // var birthMonth = prompt('What month were you born in?')
     // var birthDate = prompt('What day of the month were you born on?')
-    var ageDays = (2018 - birthYear) * 265
+    var currentYear = new Date().getFullYear()
+    var ageDays = (currentYear - birthYear) * 265
     var h1 = document.createElement('h1')
     var textAnswer = document.createTextNode('You are ' + ageDays + ' days old.')
     h1.setAttribute('id','ageInDays');
     h1.appendChild(textAnswer)
     document.getElementById('flex-box-result').appendChild(h1)
-    console.log(ageDays)
 }
 
 function reset() {
