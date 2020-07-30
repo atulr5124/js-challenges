4444f1f7256a6a739c58a20e03f351b6602dd2af
Atul Rauthan <atulrauthan7@gmail.com>
2020-07-30 13:18:30 +0530

    First working sample

--- static/js/script.js
+++ static/js/script.js
@@ -27,3 +27,64 @@ function generateCats() {
     image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small"
     div.appendChild(image)
 }
+
+// Challenge 3: Rock, Paper, Scissors
+ function rpsGame(yourChoice) {
+    var humanChoice, botChoice
+    humanChoice = yourChoice.id
+    botChoice = generateBotChoice()
+    results = decideWinner(humanChoice, botChoice)
+    message = finalMessage(results)
+    console.log(message)
+    rpsFrontEnd(humanChoice, botChoice, message)
+ }
+
+ function generateBotChoice() {
+    choices = ['rock','paper','scissors']
+    botChoiceIndex = Math.floor(Math.random() * 3)
+    return choices[botChoiceIndex]
+ }
+
+ function decideWinner(humanChoice, botChoice) {
+    if ((humanChoice == "rock" && botChoice == "paper") || (humanChoice == "paper" && botChoice == "scissors") || (humanChoice == "scissors" && botChoice == "rock")) {
+        return [0,1]
+    } else if ((humanChoice == "rock" && botChoice == "scissors") || (humanChoice == "paper" && botChoice == "rock") || (humanChoice == "scissors" && botChoice == "paper")) {
+        return [1,0]
+    } else {
+        return [1,1]
+    }
+ }
+
+ function finalMessage(results) {
+     if(results[0] == 0 && results[1] == 1) {
+         return {
+             "msg": "You lost!",
+             "color": "red"
+         }
+     } else if (results[0] == 1 && results[1] == 0 ) {
+         return {
+             "msg": "You won!",
+             "color": "green"
+         }
+     } else {
+         return {
+             "msg": "It is a tie!",
+             "color": "orange"
+         }
+     }
+ }
+
+ function rpsFrontEnd(humanChoice, botChoice, message) {
+     var resultDiv = document.getElementById('flex-box-rps-div')
+     resultDiv.innerHTML = ""
+     var humanChoiceImage = document.createElement('img')
+     humanChoiceImage.src="./static/images/"+humanChoice+".png"
+     var botChoiceImage = document.createElement('img')
+     botChoiceImage.src = "./static/images/"+botChoice+".png"
+     var resultsDeclaration = document.createElement('h2')
+     resultsDeclaration.textContent = message.msg
+     resultsDeclaration.style = "color:"+message.color
+     resultDiv.appendChild(humanChoiceImage)
+     resultDiv.appendChild(resultsDeclaration)
+     resultDiv.appendChild(botChoiceImage)
+ }
